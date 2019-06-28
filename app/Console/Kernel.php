52abf5e1eecb0bad4b92f0900e9213e19d93473f
Nizar <nizarberjawi12@gmail.com>
2019-06-28 17:21:21 +1000

    Update schedule times

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new PickUser)->everyMinute();
+        $schedule->job(new PickUser)->dailyAt('08:30');
+        $schedule->job(new PickUser)->dailyAt('13:30');
     }
 
     /**
